83dc7e67ad4d6894b9d868374005053fe3af68dc
Mantas Zambacevicius <mantas.zambacevicius@icybit.co>
2018-07-08 00:23:54 +0300

    fixed indentation

--- Models/Message.go
+++ Models/Message.go
@@ -1,7 +1,7 @@
 package Models
 
 type Message struct {
-	Id      int64  `json: "id"`
-	Message string `json: "message"`
-	Event   string `json: "event"`
+    Id      int64  `json: "id"`
+    Message string `json: "message"`
+    Event   string `json: "event"`
 }
--- Services/messagingService.go
+++ Services/messagingService.go
@@ -1,78 +1,78 @@
 package Services
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
+    "encoding/json"
+    "fmt"
+    "log"
 
-	"../Models"
-	"github.com/streadway/amqp"
+    "../Models"
+    "github.com/streadway/amqp"
 )
 
 var conn, err = amqp.Dial("amqp://guest:guest@localhost:5672/")
 var ch, chErr = conn.Channel()
 
 func GetConnection() *amqp.Connection {
-	return conn
+    return conn
 }
 
 func GetChannel() *amqp.Channel {
-	return ch
+    return ch
 }
 
 func Publish(message string, channel string, event string) {
-	q, err := ch.QueueDeclare(
-		channel,
-		false,
-		false,
-		false,
-		false,
-		nil,
-	)
-	failOnError(err, "Failed to declare a queue")
-	data := &Models.Message{Message: message, Event: "msg"}
-	b, err := json.Marshal(data)
+    q, err := ch.QueueDeclare(
+        channel,
+        false,
+        false,
+        false,
+        false,
+        nil,
+    )
+    failOnError(err, "Failed to declare a queue")
+    data := &Models.Message{Message: message, Event: "msg"}
+    b, err := json.Marshal(data)
 
-	body := string(b)
-	ch.Publish(
-		"",
-		q.Name,
-		false,
-		false,
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
-		})
+    body := string(b)
+    ch.Publish(
+        "",
+        q.Name,
+        false,
+        false,
+        amqp.Publishing{
+            ContentType: "text/plain",
+            Body:        []byte(body),
+        })
 }
 
 func Subscribe(channel string) <-chan amqp.Delivery {
-	q, err := ch.QueueDeclare(
-		channel,
-		false,
-		false,
-		false,
-		false,
-		nil,
-	)
-	failOnError(err, "Failed to declare a queue")
+    q, err := ch.QueueDeclare(
+        channel,
+        false,
+        false,
+        false,
+        false,
+        nil,
+    )
+    failOnError(err, "Failed to declare a queue")
 
-	msgs, err := ch.Consume(
-		q.Name,
-		"",
-		true,
-		false,
-		false,
-		false,
-		nil,
-	)
-	failOnError(err, "Failed to register a consumer")
+    msgs, err := ch.Consume(
+        q.Name,
+        "",
+        true,
+        false,
+        false,
+        false,
+        nil,
+    )
+    failOnError(err, "Failed to register a consumer")
 
-	return msgs
+    return msgs
 }
 
 func failOnError(err error, msg string) {
-	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
-		panic(fmt.Sprintf("%s: %s", msg, err))
-	}
+    if err != nil {
+        log.Fatalf("%s: %s", msg, err)
+        panic(fmt.Sprintf("%s: %s", msg, err))
+    }
 }
